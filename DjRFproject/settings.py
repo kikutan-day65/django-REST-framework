794c4fbf67ff0c29a559cf12f6d3826c5a7f6224
Haruto Mori <baystarsman@outlook.jp>
2023-03-08 05:06:51 +0100

    add api authentification function

--- DjRFproject/settings.py
+++ DjRFproject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
     'DjRFapp',
 ]
 
@@ -125,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
--- DjRFproject/views.py
+++ DjRFproject/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from DjRFapp.serializers import StudentSerializer
 from DjRFapp.models import Student
 
 class TestView(APIView):
+
+    permission_classes = (IsAuthenticated, )
+
     def get(self, request, *args, **kwargs):
         qs = Student.objects.all()
         student1 = qs.first()
